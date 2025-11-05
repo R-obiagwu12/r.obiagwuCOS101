c71a91b0da5357347cd20782e29292f84720c0a3
R-obiagwu12 <ronald.obiagwu@pau.edu.ng>
2025-11-05 17:22:26 +0100

    Delete life-5 directory

--- life-5/target/debug/deps/life_5.d
+++
@@ -1,5 +0,0 @@
-C:\Users\NEW USER\OneDrive\Documents\r.obiagwuCOS101\life-5\target\debug\deps\life_5.d: src\main.rs
-
-C:\Users\NEW USER\OneDrive\Documents\r.obiagwuCOS101\life-5\target\debug\deps\life_5.exe: src\main.rs
-
-src\main.rs:
