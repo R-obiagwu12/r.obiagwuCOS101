f83dd9290804891512b7c543647f153963c6a95e
R-obiagwu12 <ronald.obiagwu@pau.edu.ng>
2025-11-05 17:22:57 +0100

    Delete life-4 directory

--- life-4/target/debug/deps/life_4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\NEW USER\OneDrive\Documents\r.obiagwuCOS101\life-4\target\debug\deps\life_4.d: src\main.rs
-
-C:\Users\NEW USER\OneDrive\Documents\r.obiagwuCOS101\life-4\target\debug\deps\life_4.exe: src\main.rs
-
-src\main.rs:
--- life-4/target/debug/life-4.d
+++
@@ -1,1 +0,0 @@
-C:\Users\NEW\ USER\OneDrive\Documents\r.obiagwuCOS101\life-4\target\debug\life-4.exe: C:\Users\NEW\ USER\OneDrive\Documents\r.obiagwuCOS101\life-4\src\main.rs
