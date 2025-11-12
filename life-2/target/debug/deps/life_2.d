4ec90a7d25e921b9985af32e143f02a172f2ee9c
R-obiagwu12 <ronald.obiagwu@pau.edu.ng>
2025-11-12 17:00:27 +0100

    Delete life-2 directory

--- life-2/target/debug/deps/life_2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\NEW USER\OneDrive\Documents\r.obiagwuCOS101\life-2\target\debug\deps\life_2.d: src\main.rs
-
-C:\Users\NEW USER\OneDrive\Documents\r.obiagwuCOS101\life-2\target\debug\deps\life_2.exe: src\main.rs
-
-src\main.rs:
