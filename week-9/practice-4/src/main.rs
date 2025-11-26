2b84d22a714d340da0e5fd8ea3983d749a36dc54
unknown <ronald.obiagwu@pau.edu.ng>
2025-11-26 16:32:59 +0100

    Appendage with rust

--- week-9/practice-4/src/main.rs
+++ week-9/practice-4/src/main.rs
@@ -4,8 +4,8 @@ use std::io::Write;
 fn main() {
     let _file = std::fs::File::create("ronald.txt").expect("Create has failed");
    let mut file = OpenOptions::new().append(true).open("ronald.txt").expect("Unable to open file");
-   file.write_all("\nHello Class".as_bytes()).expect("Write failed");
-   file.write_all("\nThis is the appendage of the document".as_bytes()).expect("Write failed");
+   file.write_all("\nEverywhere go semo".as_bytes()).expect("Write failed");
+   file.write_all("\nI love this game".as_bytes()).expect("Write failed");
    println!("file append is succesful");
 
 }
