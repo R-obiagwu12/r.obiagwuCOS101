f13deace97af12f8d7b90dce3f6fb4b73ab5837f
unknown <ronald.obiagwu@pau.edu.ng>
2025-12-03 16:54:05 +0100

    Rust Structure

--- week-10/practice-7/src/main.rs
+++ week-10/practice-7/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+struct Employee {
+    name:String,
+    company:String,
+    age:u32,
 }
+
+fn main() {
+    let  emp1= Employee{
+        company:String::from("Enrst & Young"),
+        name:String::from("Ebibong Jessica"),
+        age:25,
+    };
+    println!("Name = {} \n",emp1.name);
+    println!("Company = {} \n",emp1.company);
+    println!("Age = {} \n",emp1.age);
+}
+
