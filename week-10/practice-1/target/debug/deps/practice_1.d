1fe11fcb3e4564df8a21dc3e2e51ba60a992dd47
unknown <ronald.obiagwu@pau.edu.ng>
2025-12-03 16:11:31 +0100

    Assigning variables to another variable

---
+++ week-10/practice-1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\NEW USER\OneDrive\Documents\r.obiagwuCOS101\week-10\practice-1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\NEW USER\OneDrive\Documents\r.obiagwuCOS101\week-10\practice-1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
