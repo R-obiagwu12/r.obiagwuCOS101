d2a7d810344c22bab5c9e80419c8ad8b5121a279
unknown <ronald.obiagwu@pau.edu.ng>
2025-12-03 16:38:50 +0100

    Cloning of vectors

---
+++ week-10/practice-4/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    //a list of nos
+    let v = vec![15,20,35,30,35,40];
+    print_vector(v.clone());
+    println!("{}",v[0]);
+
+}
+fn print_vector(x:Vec<i32>){
+    println!("Inside print_vector function {:?}",x);
+}
+
